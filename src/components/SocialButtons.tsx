92b1c705e2efab63eed9ab84773bd1a17f1d3e83
muhammad-munawwar <mha.codex97@gmail.com>
2026-06-20 22:01:30 +0500

    feat: add 3D coding portrait asset and animated SocialButtons component

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -27,24 +27,25 @@ const AnimatedSocialButton = ({ href, ariaLabel, children, ...props }: any) => {
 export const SocialButtons = () => {
   return (
     <div className="fixed bottom-8 right-8 z-[90] flex flex-col gap-3">
+      {/* GitHub Link */}
+      <AnimatedSocialButton href="https://github.com/muhammad-munawwar" target="_blank" rel="noreferrer" ariaLabel="GitHub">
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+        </svg>
+      </AnimatedSocialButton>
+
       {/* WhatsApp Link */}
-      <AnimatedSocialButton href="https://wa.me/4915754405511" target="_blank" rel="noreferrer" ariaLabel="WhatsApp">
+      <AnimatedSocialButton href="https://wa.me/923141304783" target="_blank" rel="noreferrer" ariaLabel="WhatsApp">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
         </svg>
       </AnimatedSocialButton>
 
-      {/* Phone dial */}
-      <AnimatedSocialButton href="tel:+4915754405511" ariaLabel="Phone">
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2v3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-        </svg>
-      </AnimatedSocialButton>
-
-      {/* Contact/Chat overlay link */}
-      <AnimatedSocialButton href="#contact" ariaLabel="Contact">
+      {/* Email Link */}
+      <AnimatedSocialButton href="mailto:muhammadmunawwar124@gmail.com" ariaLabel="Email">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+          <polyline points="22,6 12,13 2,6" />
         </svg>
       </AnimatedSocialButton>
     </div>
